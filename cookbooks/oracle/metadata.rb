73638f4bf3f6aa965526fdd19c577d7b38d0ef98
Tim Green <tgreen@opscode.com>
2012-10-09 16:17:45 -0400

    added oracle cookbook

---
+++ cookbooks/oracle/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Opscode, Inc."
+maintainer_email "tgreen@opscode.com"
+license          "All rights reserved"
+description      "Installs/Configures oracle"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
